3362200c0bef5c0438c0f397af5ebf1099cd6179
Benjamin Kyhn <benj2605@edu.easj.dk>
2020-02-17 10:52:51 +0100

    Data is stoed in Model and is used to populate the textView

---
+++ app/src/main/java/com/example/mvvm2/Model/Model.java
@@ -0,0 +1,13 @@
+package com.example.mvvm2.Model;
+
+public class Model {
+    String data = "Empty string";
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
