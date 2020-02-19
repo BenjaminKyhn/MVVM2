72517199cdba7fa7ebabb03448db06afad00602a
Benjamin Kyhn <benkyhn@hotmail.com>
2020-02-19 17:54:12 +0100

    Attempt at adding Firebase

--- app/src/main/java/com/example/mvvm2/View/AndroidView.java
+++ app/src/main/java/com/example/mvvm2/View/AndroidView.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.mvvm2.Model.Model;
+import com.example.mvvm2.Persistence.FirebaseHandler;
 import com.example.mvvm2.R;
 
 import java.util.Observable;
@@ -24,14 +26,22 @@ public class AndroidView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initialiseModulesWithFirebase();
+
         outputView = findViewById(R.id.outputView);
         inputText = findViewById(R.id.inputText);
         enterButton = findViewById(R.id.enterButton);
 
-        observeViewModel();
+        observePresenter();
     }
 
-    private void observeViewModel(){
+    private void initialiseModulesWithFirebase(){
+        FirebaseHandler dataProvider = new FirebaseHandler(null);
+        Model model = ((FirebaseHandler) dataProvider).getModel();
+        lowerCasePresenter = new LowerCasePresenter();
+    }
+
+    private void observePresenter(){
         lowerCasePresenter.addObserver(new Observer() {
             @Override
             public void update(Observable o, Object arg) {
